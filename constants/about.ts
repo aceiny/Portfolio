ed115400cb082e5841ed1b9d19a3e7f66c31e535
aceiny <yzeraibi2000@gmail.com>
2025-02-17 23:23:13 +0100

    test : check coolify auto deployment after changin ssl configs

--- constants/about.ts
+++ constants/about.ts
@@ -1,6 +1,6 @@
 export const quickStats = [
     { value: "2+", label: "Years Coding" },
-    { value: "55+", label: "Projects" },
+    { value: "52+", label: "Projects" },
     { value: "2100+", label: "Commits" },
     { value: "17+", label: "Day Streak" },
 ];
