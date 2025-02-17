040aa944921322d889df94c1d2efc0b4a499c46e
aceiny <yzeraibi2000@gmail.com>
2025-02-17 23:02:53 +0100

    test : test coolify auto deployment

--- constants/about.ts
+++ constants/about.ts
@@ -1,6 +1,6 @@
 export const quickStats = [
     { value: "2+", label: "Years Coding" },
-    { value: "60+", label: "Projects" },
+    { value: "55+", label: "Projects" },
     { value: "2100+", label: "Commits" },
     { value: "17+", label: "Day Streak" },
 ];
