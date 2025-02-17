2c3f2f1ab03ca6812ab95237bb78ff5954c2d6c7
aceiny <yzeraibi2000@gmail.com>
2025-02-17 02:47:18 +0100

    update commit count in quickStats to reflect recent contributions

--- constants/about.ts
+++ constants/about.ts
@@ -1,6 +1,6 @@
 export const quickStats = [
     { value: "2+", label: "Years Coding" },
     { value: "50+", label: "Projects" },
-    { value: "2000+", label: "Commits" },
+    { value: "2100+", label: "Commits" },
     { value: "20+", label: "Day Streak" },
 ];
