d1f6b74f8e63b85f757290ddca866de3ff9e4aba
aceiny <yzeraibi2000@gmail.com>
2025-02-17 02:41:00 +0100

    update day streak value in quickStats to reflect recent changes

--- constants/about.ts
+++ constants/about.ts
@@ -2,5 +2,5 @@ export const quickStats = [
     { value: "2+", label: "Years Coding" },
     { value: "50+", label: "Projects" },
     { value: "2000+", label: "Commits" },
-    { value: "17+", label: "Day Streak" },
+    { value: "20+", label: "Day Streak" },
 ];
