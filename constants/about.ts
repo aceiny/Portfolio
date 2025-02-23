af54264cef3de6974187a5926ec5ede192eef0ef
aceiny <aceiny.dev@gmail.com>
2025-02-23 22:10:01 +0100

    update day streak statistic to reflect current count

--- constants/about.ts
+++ constants/about.ts
@@ -2,5 +2,5 @@ export const quickStats = [
     { value: "2.5+", label: "Years Coding" },
     { value: "50+", label: "Projects" },
     { value: "2100+", label: "Commits" },
-    { value: "18+", label: "Day Streak" },
+    { value: "20+", label: "Day Streak" },
 ];
