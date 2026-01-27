f2736b953c7615bfe742fa899c8ecfdd4ca3db17
yassine <aceiny.dev@gmail.com>
2026-01-27 18:04:32 +0100

    ,

--- constants/link.ts
+++ constants/link.ts
@@ -1,1 +1,10 @@
-export const CV_LINKS: { language: string; url: string }[] = [];
+export const CV_LINKS: { language: string; url: string }[] = [
+    {
+        language: "English",
+        url: "https://drive.google.com/file/d/1J7Iz09NombOgwDF0_2BVUckFapQ3mYFC/view?usp=sharing",
+    },
+    {
+        language: "French",
+        url: "https://drive.google.com/file/d/12MDpp2-HQ11BBu4FYU6cIw2kDx03vGYl/view?usp=sharing",
+    },
+];
